28ff663638bac7f20cc48cdfd3f6159a0702d35e
MykolaKyian <kynunpodau2@gmail.com>
2024-12-07 11:25:03 -0330

    add check reqvest for CRUD operations

--- dist/src/app.js
+++ dist/src/app.js
@@ -7,15 +7,97 @@ exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const db_1 = __importDefault(require("../db/db"));
-let videoDB = db_1.default;
+let videosDB = db_1.default;
 exports.app = (0, express_1.default)(); // создать приложение
 exports.app.use(express_1.default.json()); // создание свойств-объектов body и query во всех реквестах
 exports.app.use((0, cors_1.default)()); // разрешить любым фронтам делать запросы на наш бэк
-exports.app.get('/', (req, res) => {
-    res.status(200).json({ version: '1.0' });
+//post new video
+exports.app.post('/videos', (req, res) => {
+    if (typeof req.body.title === 'string' && typeof req.body.author === 'string' && req.body.availableResolutions.length !== 0 && req.body.author.length <= 40 && req.body.title.length <= 40) {
+        const randomInt32 = () => Math.floor(Math.random() * (2 ** 32));
+        const dataPost = new Date();
+        dataPost.setDate(dataPost.getDate() + 1);
+        const newVideo = { "id": +randomInt32(),
+            "title": req.body.title,
+            "author": req.body.author,
+            "canBeDownloaded": true,
+            "minAgeRestriction": null,
+            "createdAt": new Date().toISOString(),
+            "publicationDate": dataPost.toISOString(),
+            "availableResolutions": [
+                req.body.availableResolutions
+            ] };
+        videosDB.push(newVideo);
+        res.status(201).json(newVideo);
+    }
+    else {
+        const errorsMessage = {
+            "errorsMessages": [
+                {
+                    "message": "string",
+                    "field": "string"
+                }
+            ]
+        };
+        res.status(400).json(errorsMessage);
+    }
 });
-exports.app.get('/video', (req, res) => {
-    res.status(200).send(videoDB);
+//get all videos
+exports.app.get('/videos', (req, res) => {
+    res.status(200).send(videosDB);
+});
+//get video by id
+exports.app.get('/videos/:id', (req, res) => {
+    console.log(req.params.id);
+    const videoId = videosDB.find(video => video.id === +req.params.id);
+    console.log(videoId);
+    if (videoId) {
+        res.status(200).send(videoId);
+    }
+    else {
+        res.send(404);
+    }
+});
+//put, renew video by id
+exports.app.put('/videos/:id', (req, res) => {
+    if (typeof req.body.title === 'string' && typeof req.body.author === 'string' && req.body.availableResolutions.length !== 0 && req.body.author.length <= 40 && req.body.title.length <= 40) {
+        const videoPut = videosDB.find(video => video.id === +req.params.id);
+        if (videoPut) {
+            videoPut.title = req.body.title;
+            videoPut.author = req.body.author;
+            videoPut.availableResolutions.push(...req.body.availableResolutions);
+            videoPut.canBeDownloaded = req.body.canBeDownloaded;
+            videoPut.minAgeRestriction = req.body.minAgeRestriction;
+            videoPut.publicationDate = new Date().toISOString();
+        }
+        else {
+            res.sendStatus(204);
+        }
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(400).send({
+            "errorsMessages": [
+                {
+                    "message": "string",
+                    "field": "string"
+                }
+            ]
+        });
+    }
+});
+//delete video by id
+exports.app.delete('/videos/:id', (req, res) => {
+    const videoDelete = videosDB.filter(video => video.id !== +req.params.id);
+    console.log(videosDB.length === videoDelete.length);
+    if (videosDB.length !== videoDelete.length) {
+        videosDB = videoDelete;
+        res.send(204);
+    }
+    else {
+        res.send(404);
+    }
+    res.status(200).send(videosDB);
 });
 // app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,98 @@ import express from 'express'
 import cors from 'cors'
 import db  from '../db/db'
  
-let videoDB = db;
+let videosDB = db;
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
 app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
  //post new video
 app.post('/videos', (req, res) => {
-    res.status(200).json({version: '1.0'})
+    if (typeof req.body.title === 'string' && typeof req.body.author === 'string' && req.body.availableResolutions.length !== 0 && req.body.author.length <= 40 && req.body.title.length <= 40) {
+        const randomInt32 = () => Math.floor(Math.random() * (2 ** 32));
+        const dataPost = new Date();
+        dataPost.setDate(dataPost.getDate() + 1)
+        const newVideo = {"id": +randomInt32(),
+  "title":req.body.title ,
+  "author": req.body.author,
+  "canBeDownloaded": true,
+  "minAgeRestriction": null,
+  "createdAt": new Date().toISOString(),
+  "publicationDate": dataPost.toISOString(),
+  "availableResolutions": [
+    req.body.availableResolutions
+  ]}
+    videosDB.push(newVideo)
+    res.status(201).json(newVideo)
+    } else {
+        const errorsMessage = {
+            "errorsMessages": [
+              {
+                "message": "string",
+                "field": "string"
+              }
+            ]
+          }
+        res.status(400).json(errorsMessage)
+    }
 })
 //get all videos
 app.get('/videos', (req, res) => {
 
-   res.status(200).send(videoDB)
+   res.status(200).send(videosDB)
 })
 //get video by id
-app.get('/videos', (req, res) => {
-
-    res.status(200).send(videoDB)
+app.get('/videos/:id', (req, res) => {
+    console.log(req.params.id)
+    const videoId = videosDB.find(video => video.id === +req.params.id)
+    console.log(videoId)
+    if (videoId) {
+        res.status(200).send(videoId)
+    }
+    else{
+        res.send(404)
+    }
+    
  })
 //put, renew video by id
-app.put('/videos', (req, res) => {
-    res.status(200).json({version: '1.0'})
+app.put('/videos/:id', (req, res) => {
+  if (typeof req.body.title === 'string' && typeof req.body.author === 'string' && req.body.availableResolutions.length !== 0 && req.body.author.length <= 40 && req.body.title.length <= 40) {
+    const videoPut = videosDB.find(video => video.id === +req.params.id)
+    if (videoPut) {
+      videoPut.title = req.body.title
+      videoPut.author = req.body.author
+      videoPut.availableResolutions.push(...req.body.availableResolutions)
+      videoPut.canBeDownloaded = req.body.canBeDownloaded
+      videoPut.minAgeRestriction = req.body.minAgeRestriction
+      videoPut.publicationDate = new Date().toISOString()
+    }
+    else{
+      res.sendStatus(204)
+    }
+    res.sendStatus(200)}
+    else{
+      res.sendStatus(400).send({
+        "errorsMessages": [
+          {
+            "message": "string",
+            "field": "string"
+          }
+        ]
+      })
+    }
 })
 //delete video by id
-app.delete('/videos', (req, res) => {
+app.delete('/videos/:id', (req, res) => {
+    const videoDelete = videosDB.filter(video => video.id !== +req.params.id)
+    console.log(videosDB.length === videoDelete.length)
+    if (videosDB.length !== videoDelete.length) {
+        videosDB = videoDelete
+        res.send(204)
+    }
+    else{
+        res.send(404)
+    }
 
-   res.status(200).send(videoDB)
+   res.status(200).send(videosDB)
 })
 // app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
